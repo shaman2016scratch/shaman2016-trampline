096c323c53c57998afa9cbff367d767279c51e60
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-12-31 01:41:10 +0300

    Update scratch.mjs

--- api/scratch.mjs
+++ api/scratch.mjs
@@ -2,8 +2,6 @@ import fetch from 'node-fetch';
 
 export default async (req, res) => {
   const { path } = req.query;
-  let { url_params } = req.query
-  const url_params = url_params.split('{')[1].split('}')[0]
 
   if (!path) {
     return res.status(400).json({
@@ -12,9 +10,7 @@ export default async (req, res) => {
   }
 
   try {
-    const url = `https://api.scratch.mit.edu/${path.replace(/^\//, '')}?${url_params}`;
-    console.log(`https://api.scratch.mit.edu/${path.replace(/^\//, '')}?${url_params}`)
-    console.log(url_params)
+    const url = `https://api.scratch.mit.edu/${path.replace(/^\//, '')}`;
     const response = await fetch(url);
 
     if (!response.ok) {
