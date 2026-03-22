1d77510be5a26955c8e507a0a77eb825d021c052
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-22 13:39:15 +0300

    Update telegram.mjs

--- api/telegram.mjs
+++ api/telegram.mjs
@@ -3,6 +3,13 @@ import fetch from 'node-fetch';
 export default async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const { path } = req.query;
+  const queryParams = new URLSearchParams();
+
+  for (const [key, value] of Object.entries(req.query)) {
+    if (key !== 'path') {
+      queryParams.append(key, value);
+    }
+  }
 
   if (!path) {
     return res.status(400).json({
@@ -11,8 +18,14 @@ export default async (req, res) => {
   }
 
   try {
-    const url = `https://api.telegram.org/${path.replace(/^\//, '')}`;
-    const response = await fetch(url);
+    const basePath = path.replace(/^\//, '');
+    const url = `https://api.telegram.org/${basePath}`;
+    const finalUrl = queryParams.toString()
+      ? `${url}?${queryParams.toString()}`
+      : url;
+
+    const response = await fetch(finalUrl);
+
     const data = await response.json();
 
     if (!response.ok) {
