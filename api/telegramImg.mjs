ccbbef79cf96da29a25e9d535665efe1fd0dbfea
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-22 13:30:39 +0300

    Update telegramImg.mjs

--- api/telegramImg.mjs
+++ api/telegramImg.mjs
@@ -13,11 +13,7 @@ export default async (req, res) => {
     const response = await fetch(url);
 
     if (!response.ok) {
-      return res.status(response.status).json({
-        error: `TG API error: ${response.statusText}`,
-        status: response.status,
-        requestedPath: path
-      });
+      return res.status(response.status).json(response);
     }
 
     const arrayBuffer = await response.arrayBuffer();
