1636e78782e26c5ac6051cea89c78104c58d8e1d
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-06-11 15:13:02 +0300

    Update telegram.mjs

--- api/telegram.mjs
+++ api/telegram.mjs
@@ -29,7 +29,7 @@ export default async (req, res) => {
     if (req.query.useHeaders) {
       response = await fetch(finalUrl, {
         method: req.method,
-        body: req.body+''
+        body: JSON.stringify(req.body)
       })
     } else {
       response = await fetch(finalUrl)
